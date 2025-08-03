926d7c1c2a8bc840c2e2084b8b946b1ed883cf5a
naveen2025re-star <naveen2025re@gmail.com>
2025-08-03 07:10:15 +0530

    Updated index.css

--- src/pages/ChatApp.tsx
+++ src/pages/ChatApp.tsx
@@ -183,13 +183,13 @@ export default function ChatApp() {
       />
       
       <div className="flex-1 flex flex-col min-h-0">
-        <div className="flex-1 min-h-0 overflow-hidden">
+        <div className="flex-1 min-h-0 overflow-hidden relative">
           <ChatHistory 
             messages={messages} 
             onStartNewAudit={handleStartNewAudit}
           />
         </div>
-        <div className="flex-shrink-0 p-4 bg-white border-t border-gray-200">
+        <div className="flex-shrink-0 p-4 bg-white border-t border-gray-200 relative z-10">
           <CodeInput onSubmit={handleAudit} isLoading={isLoading} />
         </div>
       </div>
